feeaa5201e863c4a4506ebf0389a9eddf729f00e
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-01-22 01:03:15 +0000

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -8,7 +8,7 @@ const middlewares = jsonServer.defaults({
 const port = process.env.PORT || 3131;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  '/tables/*': '/$1'
+  '/api/*': '/$1'
 }));
 
 server.use(router);
